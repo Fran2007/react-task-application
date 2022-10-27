ca7ab20f9e0a060e955ef2b1350567505a9204e1
Franklin <cd>
2022-10-27 09:33:32 -0600

    Se agrega el proyecto

--- src/components/TaksList.jsx
+++ src/components/TaksList.jsx
@@ -1,10 +1,9 @@
 import TaskCard from "./taskCard";
-import {useContext} from 'react'
-import {TaskContext} from '../context/TaskContext'
+import {useContext} from 'react';
+import {TaskContext} from "../context/TaskContext";
 
 function TaskList() {
-
-    const { tasks } = useContext(TaskContext)
+    const { tasks } = useContext(TaskContext);
 
   if (tasks.length === 0) {
     return <h1>No hay tareas aun</h1>;
